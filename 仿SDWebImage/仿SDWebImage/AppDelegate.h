77fbb5ab82fc1372917e7eb5395c902bfd082d58
EnjoySR <yinqiaoyin@gmail.com>
2016-09-17 16:25:56 +0800

    创建项目

---
+++ 仿SDWebImage/仿SDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿SDWebImage
+//
+//  Created by HM on 16/9/17.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿SDWebImage/仿SDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿SDWebImage
+//
+//  Created by HM on 16/9/17.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
