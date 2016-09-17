36eea7f3e168595d00a9216bd49cc055e1e82248
EnjoySR <yinqiaoyin@gmail.com>
2016-09-17 16:58:44 +0800

    重写main

--- 仿SDWebImage/仿SDWebImage/DSBWebImage/DownloaderOperation.m
+++ 仿SDWebImage/仿SDWebImage/DSBWebImage/DownloaderOperation.m
@@ -10,4 +10,11 @@
 
 @implementation DownloaderOperation
 
+// 任何操作在执行执行都会调用main方法 : 一旦队列调度操作执行,首先自动执行start方法,然后自动执行main方法
+/// 重写main方法 : 所有操作的入口 (相当于教室的门)
+- (void)main
+{
+    NSLog(@"%@",[NSThread currentThread]);
+}
+
 @end
